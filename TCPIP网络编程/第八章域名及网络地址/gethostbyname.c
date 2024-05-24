c318e586606b8d4b34b117aa485f979557a9719e
李哲 <118163257+syloe1@users.noreply.github.com>
2024-05-24 20:56:31 +0800

    Add files via upload

---
+++ TCPIP网络编程/第八章域名及网络地址/gethostbyname.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+const int BUF_SIZE = 30;
+
+void error_handling(char *message) {
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
+
+int main(int argc, char *argv[]) {
+    int i;
+    struct hostent* host;
+
+    if (argc != 2) {
+        printf("Usage: %s <addr>\n", argv[0]);
+        exit(1);
+    }
+
+    host = gethostbyname(argv[1]);
+    if (!host) {
+        error_handling("gethostbyname error");
+    }
+
+    printf("Official name: %s \n", host->h_name);
+
+    if (host->h_aliases[0]) {
+        for (i = 0; host->h_aliases[i]; i++) {
+            printf("Aliases %d: %s \n", i + 1, host->h_aliases[i]);
+        }
+    } else {
+        printf("No aliases found.\n");
+    }
+
+    printf("Address type: %s \n",
+           (host->h_addrtype == AF_INET) ? "AF_INET" : "AF_INET6");
+
+    for (i = 0; host->h_addr_list[i]; i++) {
+        printf("IP adr %d: %s \n", i + 1,
+               inet_ntoa(*(struct in_addr*)host->h_addr_list[i]));
+    }
+
+    return 0;
+}
+
