b46152c9d875f78c010c1c0f9fca3fe78c8526c2
李哲 <118163257+syloe1@users.noreply.github.com>
2024-05-27 19:43:49 +0800

    Add files via upload

---
+++ UNIX环境高级编程/第二章UNIX标准及实现/determine number.cpp
@@ -0,0 +1,25 @@
+#include <errno.h>
+#include <unistd.h>
+#include "../apue.h"
+#include "../myerr.h"
+#ifdef OPEN_MAX
+static long openmax = OPEN_MAX;
+#else 
+static long openmax = 0;
+#endif
+
+#define OPEN_MAX_GUESS 256
+
+long open_max(void) {
+    if (openmax == 0) {
+        errno = 0;
+        if ((openmax = sysconf(_SC_OPEN_MAX)) < 0) {
+            if (errno == 0) {
+                openmax = OPEN_MAX_GUESS;
+            } else {
+                err_sys("sysconf error for _SC_OPEN_MAX");
+            }
+        }
+    }
+    return openmax;
+}
