451c1a66c74e86de362c732a6e2daa07107a138b
李哲 <118163257+syloe1@users.noreply.github.com>
2024-05-29 18:34:26 +0800

    Add files via upload

---
+++ Linux高性能服务器编程/第六章高级IO函数/6-6.cpp
@@ -0,0 +1,7 @@
+//将文件描述符设置为非阻塞的
+int setnonblock(int fd) {
+	int old_option = fcntl(fd, F_GETFL);
+	int new_option = old_option | O_NONBLOCK;
+	fcntl(fd, F_SETFL, new_option);
+	return old_option;
+}
